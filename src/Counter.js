76296cde8dd501b5a14aabdc77d0ac4937342194
Geoff Vincent <geoffkvincent@gmail.com>
2018-09-13 16:04:00 -0600

    initial commit

---
+++ src/Counter.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+class Counter extends React.Component {
+  state = { value: 0 }
+
+  inc = () => {
+    this.setState({ value: this.state.value + 1 })
+  }
+
+  dec = () => {
+    this.setState({ value: this.state.value - 1 })
+  }
+
+  render() {
+    //const name = this.props.name
+    const { name } = this.props
+    //const value = this.state.value
+    const { value } = this.state
+    return (
+      <div style={{backgroundColor: name,color: 'white'}}>
+        <h1>{name} counter</h1>
+        <h1>{value}</h1>
+        <button onClick={this.inc}>+</button>
+        <button onClick={this.dec}>-</button>
+      </div>
+    )
+  }
+}
+
+export default Counter
